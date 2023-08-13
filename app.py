e9a953a164205cd69e7717d5092d78ae1284822c
IshaanSaxena313 <ishaansaxena313@gmail.com>
2023-08-13 11:00:58 +0530

    Update and delete actions working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -17,19 +17,41 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def hello_world():
-    todo = Todo(title = "First todo",desc = "This is first todo")
-    db.session.add(todo)
-    db.session.commit()
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title = title,desc = desc)
+        db.session.add(todo)
+        db.session.commit()
 
-
-    return render_template('index.html')
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo = allTodo)
     # return "Hello World"
 
-@app.route('/products')
-def products():
-    return "This is products page"
+@app.route('/update/<int:sno>',methods=['GET','POST'])
+def update(sno):
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+
+        return redirect('/')
+
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo = todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
